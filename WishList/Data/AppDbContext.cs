d673643ccbe31126148a6374b2bb69f95dadee2f
spitum <53660198+spitum@users.noreply.github.com>
2021-04-07 16:37:26 -0600

    Completed module

---
+++ WishList/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
